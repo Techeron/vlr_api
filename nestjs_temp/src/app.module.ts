c9b22b76ca484bad3a02436d37545d02a711e987
codokk101 <codokk101@gmail.com>
2023-12-27 03:52:40 -0500

    Added MongoDB Connection

--- nestjs_temp/src/app.module.ts
+++ nestjs_temp/src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { EventsModule } from './events/events.module';
 import { PlayersModule } from './players/players.module';
 import { TeamsModule } from './teams/teams.module';
@@ -6,7 +8,18 @@ import { MatchesModule } from './matches/matches.module';
 import { ApiModule } from './api/api.module';
 
 @Module({
-  imports: [EventsModule, PlayersModule, TeamsModule, MatchesModule, ApiModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    EventsModule,
+    PlayersModule,
+    TeamsModule,
+    MatchesModule,
+    ApiModule,
+  ],
   controllers: [],
 })
 export class AppModule {}
--- nestjs_temp/src/schemas/event.schema.ts
+++ nestjs_temp/src/schemas/event.schema.ts
@@ -1,13 +1,26 @@
 import * as mongoose from 'mongoose';
+import { Prop, Schema } from '@nestjs/mongoose';
 
-export const EventSchema = new mongoose.Schema({
-  type: String,
-  event_id: Number,
-  title: String,
-  date: Date,
-  status: String,
-  prize: String,
-  region: String,
-  logo: String,
-  url: String,
-});
+@Schema({
+  timestamps: true,
+})
+export class Event {
+  @Prop()
+  type: string;
+  @Prop()
+  event_id: number;
+  @Prop()
+  title: string;
+  @Prop()
+  date: Date;
+  @Prop()
+  status: string;
+  @Prop()
+  prize: string;
+  @Prop()
+  region: string;
+  @Prop()
+  logo: string;
+  @Prop()
+  url: string;
+}
